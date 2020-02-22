32f8ff97b485bc6a6810cbe3b30f8f125250c9ae
Matt Jones <matthewj866@gmail.com>
2020-02-22 22:52:47 +0000

    Added centralised include header for the sample.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 // Copyright © Matt Jones and Contributors. Licensed under the MIT Licence (MIT). See LICENCE.md in the repository root for more information.
 
-#include <NovelRT.h>
+#include <TouhouNovelRT.h>
 
 int main(int argc, char *argv[]) {
   auto runner = NovelRT::NovelRunner(0, "TouhouNovelRT");
-  runner.runNovel();
-  return 0;
+  return runner.runNovel();
 }
